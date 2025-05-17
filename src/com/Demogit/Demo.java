53ead5477dcf04e74aeacd822f4d22d97bab6248
Wang Quốc Khánh <wangquockhanh@MacBook-Air-cua-Wang.local>
2025-05-17 15:58:47 +0700

    commit 1: init source

---
+++ src/com/Demogit/Demo.java
@@ -0,0 +1,27 @@
+package com.Demogit;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello everybody!");
+        /**
+         * git init: tạo git local
+         * git remote add origin: kết nối với git origin
+         * git remote -v: kiểm tra origin
+         * git status: kiểm tra trạng thái được thay đổi
+         * git add -A: đưa tất cả các commit từ working copy vào giai đoạn staging area
+         * working copy: trạng thái của những dòng code của file chưa được commit
+         *      (commit) đơn vị của git
+         * staging area: là một khu vực trung gian nơi bạn có thể chuẩn bị (stage) những thay đổi trước khi commit vào repository.
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         */
+    }
+}
