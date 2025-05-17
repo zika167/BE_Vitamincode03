d04669488d2d43afa357df98b908242179944c98
Wang Quốc Khánh <wangquockhanh@MacBook-Air-cua-Wang.local>
2025-05-17 16:15:28 +0700

    commit 2: continue update on commit 1

--- src/com/Demogit/Demo.java
+++ src/com/Demogit/Demo.java
@@ -8,11 +8,15 @@ public class Demo {
          * git remote add origin: kết nối với git origin
          * git remote -v: kiểm tra origin
          * git status: kiểm tra trạng thái được thay đổi
-         * git add -A: đưa tất cả các commit từ working copy vào giai đoạn staging area
-         * working copy: trạng thái của những dòng code của file chưa được commit
-         *      (commit) đơn vị của git
-         * staging area: là một khu vực trung gian nơi bạn có thể chuẩn bị (stage) những thay đổi trước khi commit vào repository.
+         * git add -A: đưa tất cả các commit từ working copy vào giai đoạn STAGING AREA
+         * git commit -m "commit1: ...": commit các file vào LOCAL REPOSITORY
+         * git commit --amend -m "commit1: ...": sửa lại trên commit mới commit ( chưa push mới sửa được )
+         * git log: kiểm tra các commit trong file
          *
+         * WORKING COPY: trạng thái của những dòng code của file chưa được commit
+         *      (commit) đơn vị của git
+         * STAGING AREA: là một khu vực trung gian nơi bạn có thể chuẩn bị (stage) những thay đổi trước khi commit vào repository.
+         * LOCAL REPOSITORY:
          *
          *
          *
