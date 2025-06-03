7b08d216fcfe7df6b9ce37305584e2974df4f7b3
zika167 <ichisora167@gmail.com>
2025-06-03 22:25:49 +0700

    Class

---
+++ git03-06-2/src/com/company/SinhVien.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class SinhVien {
+    String name;
+    Boolean sex;
+    String major;
+
+    public SinhVien() {
+    }
+
+    public SinhVien(String name, Boolean sex, String major) {
+        this.name = name;
+        this.sex = sex;
+        this.major = major;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
